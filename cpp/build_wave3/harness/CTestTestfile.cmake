f3f83be3d2557c37ca7a42ef25c644245234bae7
Jonathan Embley-Riches <jonathanembleyriches@outlook.com>
2026-07-15 17:01:55 +0100

    chore: untrack scratch build dir; broaden build ignore pattern

--- cpp/build_wave3/harness/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jonat/Documents/protospec/cpp/harness
-# Build directory: C:/Users/jonat/Documents/protospec/cpp/build_wave3/harness
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
